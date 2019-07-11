967a6fd6e56013e79cecfcc3c7bad0fb30768c7b
Hendrik Knoche <hendrik.knoche@gmail.com>
2019-07-11 12:05:33 +0200

    added some more plots and anova for different conditions and HR avg

--- P8hrvavg_RRS.R
+++ P8hrvavg_RRS.R
@@ -6,6 +6,7 @@ library(readxl)
 setwd(paste(here::here("P8dataVRTV")))
 #fdf<-read.csv("fdf.csv")
 fdf<-read_excel("fdf-splitMOD.xlsx")
+fdf$condMedium<-substr(fdf$condition,1,2)
 #fdf$episode<-gsub("ArtPlayer", "intervention", fdf$episode)
 #fdf$episode<-gsub("Debrief", "debrief", fdf$episode)
 #fdf$episode<-gsub("syringe", "stressor", fdf$episode)
@@ -16,15 +17,40 @@ level_order <- c("baseline","stressor","intervention")
 
 fdfp <- summarySE(fdf, measurevar="Avg.HR", groupvars=c("episode","condition"))
 fdfr <- summarySE(fdf, measurevar="RRS", groupvars = c("episode","condition"))
-fdfBL<-fdf[fdf$episode=="baseline",c("PID","condition","Avg.HR")]
-names(fdfBL)[3]="HRatBaseline"
+fdfBL<-fdf[fdf$episode=="baseline",c("PID","Avg.HR")]
+names(fdfBL)[2]="HRatBaseline"
+fdfst<-fdf[fdf$episode=="stressor",c("PID","Avg.HR")]
+names(fdfst)[2]="HRatStressor"
+
 #sqldf("select PID, Avg.HR as HRatBaseline from fdf where ")
 fdf<-merge(fdf,fdfBL)
+fdf<-merge(fdf,fdfst)
+
 fdf$HRchangeInPercent<-1-fdf$Avg.HR/fdf$HRatBaseline
 fdf$HRchangeInBPM<-fdf$Avg.HR-fdf$HRatBaseline
+fdf$HRchangeInBPMfromStressor<-fdf$Avg.HR-fdf$HRatStressor
+fdf$HRchangeInPercentfromStressor<-1-fdf$Avg.HR/fdf$HRatStressor
+
 fdfpp<- summarySE(fdf, measurevar="HRchangeInPercent", groupvars=c("episode","condition"))
 fdfpc<- summarySE(fdf, measurevar="HRchangeInBPM", groupvars=c("episode","condition"))
+fdpst<-summarySE(fdf, measurevar="HRchangeInBPMfromStressor", groupvars=c("episode","condition"))
+fdpstpct<-summarySE(fdf, measurevar="HRchangeInPercentfromStressor", groupvars=c("episode","condition"))
 
+oneway.test(fdf[fdf$episode=="intervention",]$HRchangeInBPMfromStressor~ fdf[fdf$episode=="intervention",]$condition)
+oneway.test(fdf[fdf$episode=="intervention",]$HRchangeInPercentfromStressor~ fdf[fdf$episode=="intervention",]$condition)
+oneway.test(fdf[fdf$episode=="intervention",]$HRchangeInPercentfromStressor~ fdf[fdf$episode=="intervention",]$condMedium)
+
+qqnorm(fdf[fdf$episode=="intervention",]$HRchangeInPercentfromStressor, pch = 1, frame = FALSE)
+qqline(fdf[fdf$episode=="intervention",]$HRchangeInPercentfromStressor, col = "steelblue", lwd = 2)
+
+
+ggplot(fdpstpct[fdpstpct$episode=="intervention",], aes(x=condition, y=-100*HRchangeInPercentfromStressor,group=1, colour = condition))+
+  geom_errorbar(aes(ymin=-100*HRchangeInPercentfromStressor-100*ci, ymax=-100*HRchangeInPercentfromStressor+100*ci), width=.1) +
+  geom_point()+ylab('heart rate reduction in % from stressor')+theme_bw() + ggtitle("All Conditions")
+
+ggplot(fdpstpct[fdpstpct$episode=="intervention",], aes(x=condition, y=-100*HRchangeInPercentfromStressor,group=1, colour = condition))+
+  geom_errorbar(aes(ymin=-100*HRchangeInPercentfromStressor-100*ci, ymax=-100*HRchangeInPercentfromStressor+100*ci), width=.1) +
+  geom_point()+ylab('heart rate reduction in % from stressor')+theme_bw() + ggtitle("All Conditions")
 
 
 ggplot(fdfpc[fdfpc$episode!="stressor removal",], aes(x=episode, y=-1*HRchangeInBPM,group=1, colour = condition))+
